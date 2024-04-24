6daaa05e81d0ad01b28fc5679186f0e9dcb0fd48
biallrede <biallrede@gmail.com>
2024-04-24 16:17:01 -0300

    correção

--- Grupo cliente/main.py
+++ Grupo cliente/main.py
@@ -6,6 +6,14 @@ app = FastAPI() # criando uma instância da classe FastAPI
 
 @app.get("/grupos_cliente/{cpf_cnpj}/{id_cliente_servico}") 
 def consulta_grupos_cliente(cpf_cnpj,id_cliente_servico):
-    grupos_cliente = consulta_cliente(cpf_cnpj,id_cliente_servico)
-    grupos_cliente_json = grupos_cliente.to_json(orient='records')
-    return grupos_cliente_json
+    grupos_cliente = pd.DataFrame(consulta_cliente(cpf_cnpj,id_cliente_servico))
+    grupo = grupos_cliente['nome_tag'].tolist()
+    # print(grupos_cliente)
+    resposta = {
+    "status": "success",
+    "msg": "Dados consultados com sucesso",
+    "grupos": grupo  # Aqui incluímos os dados do grupo
+    }
+    
+    # grupos_cliente_json = grupos_cliente.to_json(orient='records')
+    return resposta
